aba20a9a8c8d1149d0391b7a3501e84103188eb1
Michał Massloch <massloch.michal@icloud.com>
2023-03-30 21:01:26 +0200

    Initial Commit

---
+++ shotTimerSwiftUI/shotTimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  shotTimerSwiftUIApp.swift
+//  shotTimerSwiftUI
+//
+//  Created by Michał Massloch on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct shotTimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
